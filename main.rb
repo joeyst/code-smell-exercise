b547b8268eb748368862027927f1d3ab93ec5ae3
joeyst <joeystenbeck@gmail.com>
2022-05-08 21:27:03 -0700

    Replace paint invocation argument with query to Button.

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 def draw_button(button, foreground_color, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(button, foreground_color - 10, background_color(is_dark_mode))
+    paint(button, button.foreground_color - 10, background_color(is_dark_mode))
   else
     # lighten foreground color for non-dark mode
-    paint(button, foreground_color + 10, background_color(is_dark_mode))
+    paint(button, button.foreground_color + 10, background_color(is_dark_mode))
   end
 end
 
